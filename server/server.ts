a8c162397e25996d3882cb15277e67d1cfdbed59
sw-itok <itok@solutionware.jp>
2024-01-22 23:47:05 +0900

    port修正〜

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3008;
 
 app.get("/api", (req: Request, res: Response) => {
   res.json({ message: "Hello, Express with TypeScript!" });
@@ -16,7 +16,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(path.join(__dirname, "../client/build", "index.html"));
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
