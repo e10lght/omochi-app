774b72baf1b3651c92bf8ba38d602accc41eed70
sw-itok <itok@solutionware.jp>
2024-01-23 23:20:19 +0900

    postgres接続設定

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,3 +1,8 @@
 export const LINE_BOT_TOKEN = process.env.TOKEN || "xxxxxx";
 export const SECRET_KEY = process.env.SECRET_KEY || "xxxxxx";
 export const PORT = process.env.PORT || 3008;
+export const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
+export const DATABASE_PORT = process.env.DATABASE_PORT || 5439;
+export const DATABASE_NAME = process.env.DATABASE_NAME || "postgres";
+export const DATABASE_PASSSWORD = process.env.DATABASE_PASSSWORD || "passw0rd";
+export const DATABASE = process.env.DATABASE || "postgres";
--- server/db/connect.ts
+++ server/db/connect.ts
@@ -1,4 +1,11 @@
 import { DataSource } from "typeorm";
+import {
+  DATABASE,
+  DATABASE_HOST,
+  DATABASE_NAME,
+  DATABASE_PASSSWORD,
+  DATABASE_PORT,
+} from "../config/config";
 import { Cleaning } from "../models/Cleaning.model";
 import { Comments } from "../models/Comments.model";
 import { Links } from "../models/Links.model";
@@ -8,11 +15,11 @@ import { Users } from "../models/Users.model";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5439,
-  username: "postgres",
-  password: "passw0rd",
-  database: "postgres",
+  host: DATABASE_HOST,
+  port: Number(DATABASE_PORT),
+  username: DATABASE_NAME,
+  password: DATABASE_PASSSWORD,
+  database: DATABASE,
   synchronize: true,
   logging: true,
   entities: [Users, Meals, Cleaning, Links, Link_Categories, Comments],
